54eb6e465e3a2a4e71792f5240439b8a5ae4b8ed
Lasse Liehu <lasse.liehu@iki.fi>
2019-05-30 12:05:44 +0300

    Add i18next

--- vuejs-reactive-i18n-test/src/main.js
+++ vuejs-reactive-i18n-test/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import i18next from 'i18next'
+import VueI18Next from '@panter/vue-i18next'
+
+Vue.use(VueI18Next)
+
+i18next.init({
+  lng: 'en',
+  resources: {
+    en: {
+      translation: {
+        "testString": "Test string"
+      }
+    }
+  }
+})
+
+const i18n = new VueI18Next(i18next)
+
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
