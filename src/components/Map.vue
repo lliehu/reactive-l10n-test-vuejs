3463f5bfc7ecdb740d8c7dad54a8edbf0ca25929
Lasse Liehu <lasse.liehu@iki.fi>
2019-09-07 21:45:22 +0300

    Add a map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,37 @@
 <template>
-<p>Here will be a map!</p>
+<div id="map"></div>
 </template>
 
 <script>
+import leaflet from 'leaflet';
+import '../../node_modules/leaflet/dist/leaflet.css';
+
+export default {
+    name: 'Map',
+    methods: {
+        initMap() {
+            this.map = leaflet.map('map').setView([61.45, 23.85], 12);
+            
+            this.tileLayer = leaflet.tileLayer(
+                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+                {
+                    maxZoom: 18,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                }
+            );
+            this.tileLayer.addTo(this.map);
+        }
+    },
+    mounted() {
+        this.initMap();
+    }
+}
 </script>
 
 <style>
+#map {
+    width: 100%;
+    height: 400px;
+    margin: auto;
+}
 </style>
