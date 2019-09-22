d5a935fbf8142cd1505c3dbae2fc61b6e53883c8
Lasse Liehu <lasse.liehu@iki.fi>
2019-09-22 21:54:06 +0300

    Remove link to Vuetify's latest release

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,6 @@
       <v-toolbar-title class="headline text-uppercase">
         {{ $t('app.name') }}
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
