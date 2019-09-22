13bcc412253f82484ede296a6f372665aa910056
Lasse Liehu <lasse.liehu@iki.fi>
2019-09-22 22:30:53 +0300

    Adjust the layout

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <v-content>
       <v-container>
         <v-row>
-          <v-col cols="12">
+          <v-col cols="6">
             <v-card width="600px" class="mx-auto">
               <v-card-text>
                 <List/>
@@ -22,16 +22,16 @@
             </v-card>
           </v-col>
 
+          <v-col cols="6">
+            <MessageLog/>
+          </v-col>
+
           <v-col cols="12">
             <v-card>
               <v-card-text>
                 <Map/>
               </v-card-text>
             </v-card>
-          </v-col>
-
-          <v-col cols="12">
-            <MessageLog/>
           </v-col>
         </v-row>
       </v-container>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -63,7 +63,7 @@ export default {
 <style>
 #map {
     width: 100%;
-    height: 400px;
+    height: 600px;
     margin: auto;
 }
 </style>
