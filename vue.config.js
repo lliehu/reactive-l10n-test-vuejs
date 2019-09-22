bbea7573163dac87beee47ed24234e4065960966
Lasse Liehu <lasse.liehu@iki.fi>
2019-09-22 19:42:28 +0300

    Reformat file

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    configureWebpack: {
-        module: {
-            rules: [
-                {
-                    test: /locales/,
-                    loader: '@alienfast/i18next-loader'
-                }
-            ]
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /locales/,
+          loader: '@alienfast/i18next-loader'
         }
+      ]
     }
+  }
 }
