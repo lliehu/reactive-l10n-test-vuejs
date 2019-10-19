a763596f37d1a89f024d25ba9fa3aa1b8ec41951
Lasse Liehu <lasse.liehu@iki.fi>
2019-10-19 22:39:59 +0300

    Fix whitespace

--- src/components/MessageLog.vue
+++ src/components/MessageLog.vue
@@ -1,5 +1,5 @@
 <template>
-<v-card width="600px" class="mx-auto">
+  <v-card width="600px" class="mx-auto">
     <v-card-title>{{ $t('message_log.title') }}</v-card-title>
     <v-card-text>
         <p v-for="(message, index) in messages" v-bind:key="index">
@@ -10,22 +10,21 @@
             {{ $t(message.messageId) }}
         </p>
     </v-card-text>
-</v-card>
+  </v-card>
 </template>
 
 <script>
 export default {
-    name: 'MessageLog',
-    computed: {
-        language() {
-            return this.$root.$data.language;
-        },
-        messages() {
-            return this.$root.$data.messageLog.messageList;
-        }
+  name: 'MessageLog',
+  computed: {
+    language() {
+      return this.$root.$data.language
+    },
+    messages() {
+      return this.$root.$data.messageLog.messageList
     }
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>
