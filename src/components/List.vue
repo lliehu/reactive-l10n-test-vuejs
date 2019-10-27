5be6d7a4cf10503b84b8f90ff9d122cd244dae20
Lasse Liehu <lasse.liehu@iki.fi>
2019-10-27 13:50:07 +0200

    Fix whitespace

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,25 +1,25 @@
 <template>
-    <div>
-        <!-- TODO FIXME Do not use parseInt. -->
-        <span>{{ $t("cat_count", { context: gender, count: parseInt(catCount)} ) }}</span>
-    </div>
+  <div>
+    <!-- TODO FIXME Do not use parseInt. -->
+    <span>
+      {{ $t('cat_count', { context: gender, count: parseInt(catCount) }) }}
+    </span>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'List',
-        props: {
-        },
-        computed: {
-            gender() {
-                return this.$root.$data.gender;
-            },
-            catCount() {
-                return this.$root.$data.catCount;
-            }
-        }
+export default {
+  name: 'List',
+  props: {},
+  computed: {
+    gender() {
+      return this.$root.$data.gender
+    },
+    catCount() {
+      return this.$root.$data.catCount
     }
+  }
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/TargetPronounChanger.vue
+++ src/components/TargetPronounChanger.vue
@@ -1,29 +1,38 @@
 <template>
-    <div>
-        <v-btn @click="switchPronounTo('')" :color="!!!gender ? 'success' : ''">{{ $t('gendered_pronoun_object') }}</v-btn>
-        <v-btn @click="switchPronounTo('female')" :color="gender === 'female' ? 'success' : ''">{{ $t('gendered_pronoun_object', {context: 'female'}) }}</v-btn>
-        <v-btn @click="switchPronounTo('male')" :color="gender === 'male' ? 'success' : ''">{{ $t('gendered_pronoun_object', {context: 'male'}) }}</v-btn>
-    </div>
+  <div>
+    <v-btn @click="switchPronounTo('')" :color="!!!gender ? 'success' : ''">
+      {{ $t('gendered_pronoun_object') }}
+    </v-btn>
+    <v-btn
+      @click="switchPronounTo('female')"
+      :color="gender === 'female' ? 'success' : ''"
+    >
+      {{ $t('gendered_pronoun_object', { context: 'female' }) }}
+    </v-btn>
+    <v-btn
+      @click="switchPronounTo('male')"
+      :color="gender === 'male' ? 'success' : ''"
+    >
+      {{ $t('gendered_pronoun_object', { context: 'male' }) }}
+    </v-btn>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'TargetPronounChanger',
-        props: {
-
-        },
-        methods: {
-            switchPronounTo(newPronoun = '') {
-                this.$root.$data.gender = newPronoun;
-            }
-        },
-        computed: {
-            gender() {
-                return this.$root.$data.gender;
-            }
-        }
+export default {
+  name: 'TargetPronounChanger',
+  props: {},
+  methods: {
+    switchPronounTo(newPronoun = '') {
+      this.$root.$data.gender = newPronoun
     }
+  },
+  computed: {
+    gender() {
+      return this.$root.$data.gender
+    }
+  }
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
