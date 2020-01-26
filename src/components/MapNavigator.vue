39d18767a0fa76ed80a3cbe4bae1e639f1e011aa
Lasse Liehu <lasse.liehu@iki.fi>
2020-01-26 20:43:18 +0200

    Add a component that displays the autocomplete field
    Forgot to add the file in the previous commit.

---
+++ src/components/MapNavigator.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card width="600px" class="mx-auto">
+    <v-card-text>
+      {{ $t('navigate_map_to_label') }}
+      <v-autocomplete
+        v-model="selectedPlace"
+        :items="items"
+        item-text="name"
+        item-value="coordinates"
+      />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        @click="$emit('navigate-map', selectedPlace)"
+        :disabled="!!!selectedPlace"
+      >
+        {{ $t('navigate_map_button') }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'MapNavigator',
+  data: function() {
+    return {
+      selectedPlace: null,
+      items: [
+        {
+          name: 'Hervanta',
+          coordinates: [61.45, 23.85]
+        },
+        {
+          name: 'Kaleva',
+          coordinates: [61.498642, 23.800743]
+        }
+      ]
+    }
+  },
+  props: {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped></style>
