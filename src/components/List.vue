5085089ceed76d69da0fd4c058bc23fcd42c4308
Lasse Liehu <lasse.liehu@iki.fi>
2020-05-30 16:45:24 +0300

    Set wanted radix to parseInt call
    Reason: The default is not 10.

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- TODO FIXME Do not use parseInt. -->
     <span>
-      {{ $t('cat_count', { context: gender, count: parseInt(catCount) }) }}
+      {{ $t('cat_count', { context: gender, count: parseInt(catCount, 10) }) }}
     </span>
   </div>
 </template>
